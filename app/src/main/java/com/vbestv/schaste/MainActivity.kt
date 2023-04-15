5a07e1874351cd900db335e91b9b3e8b8fe9c08c
VbestV <vbestv81@gmail.com>
2023-04-15 15:52:39 +0300

    Добавлено возможность отобразить место на карте

--- app/src/main/java/com/vbestv/schaste/MainActivity.kt
+++ app/src/main/java/com/vbestv/schaste/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.vbestv.schaste
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vbestv.schaste.databinding.ActivityMainBinding
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("QueryPermissionsNeeded")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,6 +22,14 @@ class MainActivity : AppCompatActivity() {
         binding.mainButton.setOnClickListener{
             val intent = Intent(this,GalleryActivity::class.java)
             startActivity(intent)
+        }
+        binding.mainButton3.setOnClickListener {
+            val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=56.01205284934431, 92.87463471078709(Пр.Мира 49)"))
+            mapIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            if (mapIntent!=null){
+                startActivity(mapIntent)
+            }
         }
     }
 }
